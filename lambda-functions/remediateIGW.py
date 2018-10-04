92aa518c6608271f4e5e3340a2d9d073c4de4f6b
Rob <szotrj@rsi.fios-router.home>
2018-10-03 22:18:23 -0400

    Refactored logic for handling multiple messages

--- lambda-functions/remediateIGW.py
+++ lambda-functions/remediateIGW.py
@@ -5,13 +5,23 @@ from __future__ import print_function
 from botocore.exceptions import ClientError
 
 import os, json, boto3
+import botocore.config
+
+# Set retries to 0
+cfg = botocore.config.Config(retries={'max_attempts': 0})
+client = boto3.client('lambda', config=cfg)
 
 # Get region identifier
 session = boto3.session.Session()
 region = session.region_name
 
+# Get current account id
+sts = boto3.client('sts')
+currAccountId = sts.get_caller_identity().get('Account')
+
+# Set SNS topic ARN
 sns = boto3.client('sns')
-sns_topic = 'arn:aws:sns:' + region + ':AccountId:IGW_Change'
+sns_topic = 'arn:aws:sns:' + region + ':' + AccountId + ':topic-name'
 
 # ===============================================================================
 
@@ -21,14 +31,11 @@ def lambda_handler(event, context):
     customerAcct = event['account']
 
     # Assume cross-account role in client systems (target) account to run function
-    sts = boto3.client('sts')
     assumedRoleObject = sts.assume_role(
         DurationSeconds=3600,
         RoleArn='arn:aws:iam::' + customerAcct + ':role/roleName',
         RoleSessionName='remediateIGW',
     )
-    #print(assumedRoleObject)
-    #print(boto3.client('sts').get_caller_identity()['Account'])
 
     credentials = assumedRoleObject['Credentials']
     ak=credentials['AccessKeyId']
@@ -41,37 +48,58 @@ def lambda_handler(event, context):
     print('Detected ' + event['detail']['eventName'] + ' API call')
     if (event['detail']['eventName'] == 'CreateInternetGateway'):
         IgwId=event['detail']['responseElements']['internetGateway']['internetGatewayId']
-        print(IgwId)
+        try:
+            response = ec2.describe_internet_gateways(
+                InternetGatewayIds=[
+                    IgwId
+                ]
+            )
+            try:
+                VpcId=response['InternetGateways'][0]['Attachments'][0]['VpcId']
+                state=response['InternetGateways'][0]['Attachments'][0]['State']
+                print('IGW State: ' + state)
+            except:
+                pass
+        except ClientError as e:
+            print('Error code: ' + e.response['Error']['Code'])
+            print('HTTP status code: ' + str(e.response['ResponseMetadata']['HTTPStatusCode']))
+            print('IGW may have already been deleted.')
     elif (event['detail']['eventName'] == 'AttachInternetGateway'):
         IgwId=event['detail']['requestParameters']['internetGatewayId']
         VpcId=event['detail']['requestParameters']['vpcId']
-        print(IgwId)
-        print(VpcId)
-
-    # Check for VPC attachment (add try/except for non-existant IGW)
-    response = ec2.describe_internet_gateways(
-        InternetGatewayIds=[
-            IgwId
-        ]
-    )
-
-    if response['InternetGateways'][0]['Attachments']:
-        print('IGW State: ' + response['InternetGateways'][0]['Attachments'][0]['State'])
-        print('IGW attached to VPC ID: ' + response['InternetGateways'][0]['Attachments'][0]['VpcId'])
+        try:
+            response = ec2.describe_internet_gateways(
+                InternetGatewayIds=[
+                    IgwId
+                ]
+            )
+            state=response['InternetGateways'][0]['Attachments'][0]['State']
+            print('IGW State: ' + state)
+        except ClientError as e:
+            print('Error code: ' + e.response['Error']['Code'])
+            print('HTTP status code: ' + str(e.response['ResponseMetadata']['HTTPStatusCode']))
+            print('IGW may have already been deleted.')
+            exit(1)
+    try:
+        VpcId
+        print('IGW (' + IgwId + ') attached to VPC (' + VpcId + ')')
         print('Detaching IGW from VPC...')
         response = ec2.detach_internet_gateway(
             InternetGatewayId=(IgwId),
             VpcId=(VpcId)
         )
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                print('Detach successful')
-        #print(response)
+            print('Detach IGW successful')
+        else:
+            print('Detach IGW failed. Check resource.')
         print('Deleting IGW...')
         response = ec2.delete_internet_gateway(
-            InternetGatewayId=(event['detail']['requestParameters']['internetGatewayId'])
+            InternetGatewayId=(IgwId)
         )
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                print('Delete successful')
+            print('Delete IGW successful')
+        else:
+            print('Delete IGW failed. There may be public IPs associated.')
 
         # SNS notification
         try:
@@ -82,13 +110,14 @@ def lambda_handler(event, context):
                 )
         except ClientError as e:
             print("SNS failure: %s" %e)
+    except:
         print('IGW not attached. Deleting IGW: ' + IgwId)
         try:
             response = ec2.delete_internet_gateway(
                 InternetGatewayId=(IgwId)
             )
             if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                print('Delete successful')
+                print('Delete IGW successful')
 
                 # SNS notification
                 try:
@@ -100,26 +129,5 @@ def lambda_handler(event, context):
                 except ClientError as e:
                     print("SNS failure: %s" %e)
         except ClientError as e:
-            print(e.response['Error']['Code'])
-            print(e.response['ResponseMetadata']['HTTPStatusCode'])
-    else:
-        print('IGW not attached. Deleting IGW: ' + IgwId)
-        try:
-            response = ec2.delete_internet_gateway(
-                InternetGatewayId=(IgwId)
-            )
-            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                print('Delete successful')
-
-                # SNS notification
-                try:
-                    response = sns.publish(
-                        TargetArn = sns_topic,
-                        Subject = 'Security Alert: IGW modification detected in ' + customerAcct,
-                        Message = 'An unsanctioned Internet Gateway (' + IgwId + ') was automatically deleted.'
-                )
-                except ClientError as e:
-                    print("SNS failure: %s" %e)
-        except ClientError as e:
-            print(e.response['Error']['Code'])
-            print(e.response['ResponseMetadata']['HTTPStatusCode'])
+            print('Error code: ' + e.response['Error']['Code'])
+            print('HTTP status code: ' + str(e.response['ResponseMetadata']['HTTPStatusCode']))
