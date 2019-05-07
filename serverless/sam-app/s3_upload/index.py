98544c104e14f63441cecc12ec9b753b060570e9
szotrj <szotrj@gmail.com>
2019-05-06 23:49:59 -0400

    Initial commit, serverless practice

---
+++ serverless/sam-app/s3_upload/index.py
@@ -0,0 +1,46 @@
+import logging
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+def create_presigned_post(bucket_name, object_name, fields=None, conditions=None, expiration=3600):
+    """Generate a presigned URL S3 POST request to upload a file
+
+    :param bucket_name: string
+    :param object_name: string
+    :param fields: Dictionary of prefilled form fields
+    :param conditions: List of conditions to include in the policy
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Dictionary with the following keys:
+        url: URL to post to
+        fields: Dictionary of form fields and values to submit with the POST
+    :return: None if error.
+    """
+
+    # Generate a presigned S3 POST URL
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_post(bucket_name,
+                object_name,
+                Fields=fields,
+                Conditions=conditions,
+                ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL and required fields
+    return response
+
+def lambda_handler(event, context):
+    print(json.dumps(event))
+    bucket_name = event['queryStringParameters']['bucketName']
+    object_name = event['queryStringParameters']['objectName']
+    response = create_presigned_post(bucket_name, object_name, None, None)
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "url": response['url'] + response['fields']['key'] + '?signature=' + response['fields']['signature'] + '&AWSAccessKeyId=' + response['fields']['AWSAccessKeyId'] + '&x-amz-security-token=' + response['fields']['x-amz-security-token']
+        })
+    }
