8a7a48503a6755444c613b4fc17652a2c5bc4d0c
Rob Szot <szotrj@gmail.com>
2021-02-22 00:18:47 -0500

    cleanup

--- cloudformation/cloudtrail/subscribeSQSToSNS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ACCOUNTS=
-CENTRALACCOUNTID=
-
-for id in `cat $ACCOUNTS`; do
-	echo "aws sns subscribe --topic-arn arn:aws-us-gov:sns:us-gov-west-1:$id:cloudtrail --notification-endpoint arn:aws-us-gov:sqs:us-gov-west-1:$CENTRALACCOUNTID:SplunkQueue --protocol sqs"
-done
--- scripts/applyS3PublicAccessBlock.sh
+++
@@ -1,1 +0,0 @@
-aws s3control put-public-access-block --account-id $(aws sts get-caller-identity --query 'Account' --output text) --public-access-block-configuration BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true
